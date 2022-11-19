2ed6f66a7b38d7ca96bd397b20cfac84d9ff2bf8
Guilherme <bastosguilherme12@gmail.com>
2022-11-19 08:59:29 -0300

    Implementando e ajustando armazenamento interno e externo

---
+++ app/src/main/java/com/example/covidapp/Constants.java
@@ -0,0 +1,9 @@
+package com.example.covidapp;
+
+public final class Constants {
+
+    public enum Type { INTERNAL, EXTERNAL}
+    public static final String STORAGE_TYPE = "storage_type";
+    public static final String FILE_NAME = "arquivo.txt";
+
+}
